6ced48f360c24ff3a2e736878e990298875a9f86
Dandelion <494933900@qq.com>
2019-08-04 22:33:39 +0800

    添加手动输入ID与保存文件夹的功能

--- Crawl-ZhihuImage.py
+++ Crawl-ZhihuImage.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import requests
 import json
 import time
@@ -12,12 +14,35 @@ UrlDataCnt = 1
 SaveImageCnt = 1
 ObjectUrlPage = 5
 
-DirPath = "download-zhihu-image"
+QuestionId = 1
+DefaultFolder = "download-zhihu-image"
+DownloadFolder = "NewFolder"
+
+def SelectModel():
+    DownloadMode = input("please input download model(default model is 0, ID model is 1):")
+    #print("DownloadMode is ",DownloadMode)
+    return DownloadMode
+
+def DefualtMode():
+    global DownloadFolder
+    global QuestionId
+    DownloadFolder = DefaultFolder      # default save folder
+    QuestionId = 34243513               # default id
+
+def InputMessage():
+    global QuestionId
+    global DownloadFolder
+
+    QuestionId = input("please input id:")
+    DownloadFolder = input("please input folder name:")
+
+    # print("Id:", QuestionId)
+    # print("folder name:", DownloadFolder)
 
 def MakeDir():
-    if not os.path.exists(DirPath):
+    if not os.path.exists(DownloadFolder):
         print("create the path successful")
-        os.mkdir(DirPath)
+        os.mkdir(DownloadFolder)
     else:
         print("the path is exist")
  
@@ -65,15 +90,13 @@ def ParseHtmlData(html):
     
     except Exception as e:
         print("error")
-        #print(comment)
-        #print(e)
         
 def SaveImage(Image):
     global SaveImageCnt
     ImageName = str(SaveImageCnt) + ".jpg"
     SaveImageCnt = SaveImageCnt + 1
 
-    ImagePath = os.path.join(DirPath, ImageName)
+    ImagePath = os.path.join(DownloadFolder, ImageName)
     with open(ImagePath, 'wb') as  f:
         print('Output:', ImagePath)
         f.write(Image)
@@ -92,9 +115,10 @@ def ParseImageUrlData(UrlData):
         UrlDataCnt += 1     
 
 def GetObjectUrl():
-    global ObjectUrlPage
+    # print("ObjectUrlPage:", ObjectUrlPage)
+    # print("QuestionId:", QuestionId)
 
-    ObjectUrl = "https://www.zhihu.com/api/v4/questions/34243513/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset="+str(ObjectUrlPage)+"&platform=desktop&sort_by=default"
+    ObjectUrl = "https://www.zhihu.com/api/v4/questions/"+str(QuestionId)+"/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset="+str(ObjectUrlPage)+"&platform=desktop&sort_by=default"
 
     return ObjectUrl
  
@@ -112,8 +136,6 @@ def DataProcessing():
       
     # Traverse all URLs
     while(ObjectUrlPage < UrlTotals):
-        print("ObjectUrlPage:",ObjectUrlPage)
-
         # get data from object-url
         Html = GetHtmlData(ObjectUrl)
         ImageUrlData = ParseHtmlData(Html)
@@ -125,10 +147,14 @@ def DataProcessing():
         
     
 if __name__ == '__main__':
-    # create folder 
-    MakeDir()
+    Model = SelectModel()       # select download model
+    if Model == '1':
+        InputMessage()          # input message from you
+    else:
+        DefualtMode()  
 
-    # data processing 
-    DataProcessing()
+    MakeDir()           # create folder 
+    
+    DataProcessing()    # data processing 
 
     print("finish")
